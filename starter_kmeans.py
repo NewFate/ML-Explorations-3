a49b6848449094be7f5662391d357886d597f652
Ronald Ong <ronald.ong@mail.utoronto.ca>
2019-04-01 18:53:47 -0400

    Added part 1.3

--- starter_kmeans.py
+++ starter_kmeans.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import helper as hlp
-import collections, numpy
 
 # Loading data
 data = np.load('data2D.npy')
@@ -10,7 +9,7 @@ data = np.load('data2D.npy')
 [num_pts, dim] = np.shape(data)
 
 
-is_valid = False
+is_valid = True
 
 # For Validation set
 if is_valid:
@@ -55,19 +54,19 @@ def buildGraph(input_data, cluster_size):
     
     return input_x, centroids, pair_distance, loss, optimizer, prediction, num_data
     
-def plot_scatter(k, traindata, label=None):
+def part_1_plot(k, traindata, cluster):
 
-    color_list = ["r","g","b","c","m"]
-    print (label.shape)
+    cluster_color = ["r","g","b","c","m"]
+    print (cluster.shape)
     print(traindata.shape)
-    clabel = []
-    for i in range(len(label)):
-        clabel.append(color_list[label[i]])
+    color_of_points = []
+    for i in range(len(cluster)):
+        color_of_points.append(cluster_color[cluster[i]])
     plt.figure()
     plt.title("Classification of points with {} clusters".format(k))
     plt.xlim([-4,5])
     plt.ylim([-5,3])
-    plt.scatter(traindata[:,0], traindata[:,1], c=clabel, marker='.', s=1)
+    plt.scatter(traindata[:,0], traindata[:,1], c=color_of_points, marker='.', s=1)
 
     #plt.savefig("./output_pic/{}_cluster_plot_{}.png".format(k, is_valid))
     
@@ -79,9 +78,10 @@ def find_distribution (train_prediction, k):
     
 def main():
     
-    k_value = 5
+    k_value = 1
     #loss values
     train_loss_list = []
+    val_train_loss_list = []
     
     #Build the graph
     input_x, centroids, pair_distance, loss, optimizer, prediction, num_data = buildGraph(data, k_value)
@@ -93,27 +93,40 @@ def main():
         # Loop over number of epochs
         for epoch in range(1000):
             feed_dict_train = {input_x: data, num_data: data.shape[0]}
-            
+            feed_dict_train_val = {input_x: val_data, num_data: val_data.shape[0]}
             #Run the optimizer
             sess.run(optimizer, feed_dict=feed_dict_train)
             
             loss_value, train_prediction = sess.run([loss, prediction], feed_dict=feed_dict_train)
+            
+            val_loss_value, val_train_prediction = sess.run([loss, prediction], feed_dict=feed_dict_train_val)
             #print(train_prediction)
             train_loss_list.append(loss_value)
-            print(loss_value)
+            val_train_loss_list.append(loss_value)
+            print("Training data loss: "+str(loss_value))
+            print("Valid data loss: " + str(val_loss_value))
      
 
     plt.figure(1)
     plt.plot(train_loss_list, c='b')
-    plt.title("Cluster Size of " + str(k_value))
+    plt.title("Training data loss: Cluster Size of " + str(k_value))
     plt.grid()
     plt.legend(loc='best')
     plt.xlabel('Iteration')
     plt.ylabel('Loss') 
     
+    plt.figure(2)
+    plt.plot(val_train_loss_list, c='g')
+    plt.title("Validation data loss: Cluster Size of " + str(k_value))
+    plt.grid()
+    plt.legend(loc='best')
+    plt.xlabel('Iteration')
+    plt.ylabel('Loss')
     
     
-    plot_scatter(k_value, data, label= train_prediction)  
+    
+    part_1_plot(k_value, data, train_prediction) 
+    part_1_plot(k_value, val_data, val_train_prediction) 
     print(find_distribution(train_prediction, k_value)) 
 
 if __name__ == "__main__":
