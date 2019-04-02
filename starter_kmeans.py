145075ba75b7a7dc867685771a02ae4586640a62
shovon77 <shovonmahmud@Shovons-MacBook-Pro-4.local>
2019-04-02 16:44:58 -0400

    K_means complete

--- starter_kmeans.py
+++ starter_kmeans.py
@@ -2,10 +2,17 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import helper as hlp
+import collections
+from random import randint
+
+colors = []
+
+for i in range(50):
+    colors.append('#'+'%06X' % randint(0, 0xFFFFFF))
 
 # Loading data
-data = np.load('data2D.npy')
-#data = np.load('data100D.npy')
+#data = np.load('data2D.npy')
+data = np.load('data100D.npy')
 [num_pts, dim] = np.shape(data)
 
 
@@ -49,19 +56,19 @@ def buildGraph(input_data, cluster_size):
     prediction = tf.argmin(pair_distance,1)
     #Optimizer
     with tf.name_scope("optimizer"):
-        optimizer = tf.train.AdamOptimizer(learning_rate=0.0075, beta1=0.9, beta2=0.99, epsilon=1e-5).minimize(loss)
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.5, beta1=0.9, beta2=0.99, epsilon=1e-5).minimize(loss)
     
     return input_x, centroids, pair_distance, loss, optimizer, prediction, num_data
     
-def part_1_plot(k, traindata, cluster):
+def part_1_plot(k, traindata, cluster, figure_number):
 
-    cluster_color = ["r","g","b","c","m"]
-    print (cluster.shape)
-    print(traindata.shape)
+    #cluster_color = ["r","g","b","c","m"] 
+    #print (cluster.shape)
+    #print(traindata.shape)
     color_of_points = []
     for i in range(len(cluster)):
-        color_of_points.append(cluster_color[cluster[i]])
-    plt.figure()
+        color_of_points.append(colors[cluster[i]])
+    plt.figure(figure_number)
     plt.title("Classification of points with {} clusters".format(k))
     plt.xlim([-4,5])
     plt.ylim([-5,3])
@@ -77,56 +84,78 @@ def find_distribution (train_prediction, k):
     
 def main():
     
-    k_value = 1
-    #loss values
-    train_loss_list = []
-    val_train_loss_list = []
+    cluster_values = [30]
+    figure_number = 0
+    final_train_loss_values = []
+    final_valid_loss_values = []
+    final_distribution = []
     
-    #Build the graph
-    input_x, centroids, pair_distance, loss, optimizer, prediction, num_data = buildGraph(data, k_value)
-
-    with tf.Session() as sess:
-    # Initialize all variables
-        sess.run(tf.global_variables_initializer())
+    for k_value in cluster_values:
+        #loss values
+        train_loss_list = []
+        val_train_loss_list = []
+        train_loss_value = 0
+        val_loss_value = 0
+    
+        #Build the graph
+        input_x, centroids, pair_distance, loss, optimizer, prediction, num_data = buildGraph(data, k_value)
+    
+        with tf.Session() as sess:
+        # Initialize all variables
+            sess.run(tf.global_variables_initializer())
+            
+            # Loop over number of epochs
+            for epoch in range(500):
+                feed_dict_train = {input_x: data, num_data: data.shape[0]}
+                feed_dict_train_val = {input_x: val_data, num_data: val_data.shape[0]}
+                #Run the optimizer
+                sess.run(optimizer, feed_dict=feed_dict_train)
+                
+                train_loss_value, train_prediction = sess.run([loss, prediction], feed_dict=feed_dict_train)
+                
+                val_loss_value, val_train_prediction = sess.run([loss, prediction], feed_dict=feed_dict_train_val)
+                #print(train_prediction)
+                train_loss_list.append(train_loss_value)
+                val_train_loss_list.append(val_loss_value)
+                #print("Training data loss: "+str(train_loss_value))
+                #print("Valid data loss: " + str(val_loss_value))
+         
         
-        # Loop over number of epochs
-        for epoch in range(1000):
-            feed_dict_train = {input_x: data, num_data: data.shape[0]}
-            feed_dict_train_val = {input_x: val_data, num_data: val_data.shape[0]}
-            #Run the optimizer
-            sess.run(optimizer, feed_dict=feed_dict_train)
-            
-            loss_value, train_prediction = sess.run([loss, prediction], feed_dict=feed_dict_train)
-            
-            val_loss_value, val_train_prediction = sess.run([loss, prediction], feed_dict=feed_dict_train_val)
-            #print(train_prediction)
-            train_loss_list.append(loss_value)
-            val_train_loss_list.append(loss_value)
-            print("Training data loss: "+str(loss_value))
-            print("Valid data loss: " + str(val_loss_value))
-     
-
-    plt.figure(1)
-    plt.plot(train_loss_list, c='b')
-    plt.title("Training data loss: Cluster Size of " + str(k_value))
-    plt.grid()
-    plt.legend(loc='best')
-    plt.xlabel('Iteration')
-    plt.ylabel('Loss') 
-    
-    plt.figure(2)
-    plt.plot(val_train_loss_list, c='g')
-    plt.title("Validation data loss: Cluster Size of " + str(k_value))
-    plt.grid()
-    plt.legend(loc='best')
-    plt.xlabel('Iteration')
-    plt.ylabel('Loss')
-    
-    
-    
-    part_1_plot(k_value, data, train_prediction) 
-    part_1_plot(k_value, val_data, val_train_prediction) 
-    print(find_distribution(train_prediction, k_value)) 
+        final_train_loss_values.append(train_loss_value)
+        final_valid_loss_values.append(val_loss_value)
+        
+        plt.figure(k_value)
+        plt.plot(train_loss_list, c='b')
+        plt.title("Training data loss: Cluster Size of " + str(k_value))
+        plt.grid()
+        plt.legend(loc='best')
+        plt.xlabel('Iteration')
+        plt.ylabel('Loss') 
+        
+        plt.figure(k_value+7)
+        plt.plot(val_train_loss_list, c='g')
+        plt.title("Validation data loss: Cluster Size of " + str(k_value))
+        plt.grid()
+        plt.legend(loc='best')
+        plt.xlabel('Iteration')
+        plt.ylabel('Loss')
+        
+        
+        
+        part_1_plot(k_value, data, train_prediction, figure_number)
+        figure_number += 1
+        part_1_plot(k_value, val_data, val_train_prediction, figure_number) 
+        final_distribution.append(find_distribution(train_prediction, k_value))
+        final_distribution.append(find_distribution(val_train_prediction, k_value))
+        
+    for i in final_train_loss_values:
+        print("Train loss: "+str(i))
+        
+    for i in final_valid_loss_values:
+        print("Valid loss: "+str(i))
+        
+    print(final_distribution)
+        
 
 if __name__ == "__main__":
     main()
